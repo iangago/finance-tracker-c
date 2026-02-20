3dc8297fe03fe4e9df6589c02b2ea8d89098b4cc
iangago <iangago20@gmail.com>
2026-02-20 12:50:14 -0300

    feat: implemented bank structure

---
+++ src/bank.h
@@ -0,0 +1,20 @@
+#ifndef BANK_H
+#define BANK_H
+
+#include "queue.h"
+#include "stack.h"
+
+typedef enum{
+    OPTION1 = 1, OPTION2 = 2, OPTION3 = 3, OPTION4 = 4, OPTION5 = 0
+}Choice;
+
+Choice readOption(void);
+int stringToInt(const char *input, int *out);
+int checkInputInt(int *value);
+void runBank(void);
+void addCustomer();
+void serveCustomer();
+void showQueue(Queue *q);
+void showHistory(Stack *s);
+
+#endif
